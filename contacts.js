6cbb56639f1ed71b13a7158dff7ae6ec00c3f972
Roman Vildanov <100133828+Vildanov-Roman@users.noreply.github.com>
2022-12-22 13:30:16 +0200

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,50 @@
+const { nanoid } = require("nanoid");
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = "./db/contacts.json";
+const filePath = path.join(__dirname, contactsPath);
+
+const listContacts = async () => {
+  const data = await fs.readFile(filePath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(
+    (contact) => contact.id === contactId.toString()
+  );
+  if (!result) {
+    return null;
+  }
+  return result;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(
+    (contact) => contact.id === contactId.toString()
+  );
+  if (index === -1) {
+    return null;
+  }
+  const result = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(filePath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
